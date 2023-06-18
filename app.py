440f30addd2b3c18b72b22144464241043dd63e8
Kool-Cool <omgrabpoint@gmail.com>
2023-06-19 00:33:23 +0530

    Database
    use following commands in terminal (python) to make database
    
    >>>python app.py
    
    >>> python
    
    >>>from app import db
    
    >>>db.create_all()
    
    For those of you who faced the database creation error, do the following:
    
    >>> from app import app, db
    >>> app.app_context().push()
    >>> db.create_all()

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
+from datetime import datetime
+from sqlalchemy import Integer , String, DateTime
+
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    
+    id = db.Column(Integer , primary_key=True)
+    content = db.Column(String(200),nullable = False)
+    date_created = db.Column(DateTime , default = datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
+    
+    
+    
 # Index  route
 @app.route("/")
 def index():
