0083fbf8d5e362ce2f51d1797c77ebaf270153b6
Kool-Cool <omgrabpoint@gmail.com>
2023-06-19 01:03:58 +0530

    Update and delete

--- app.py
+++ app.py
@@ -1,52 +1,148 @@
-from flask import Flask
-from flask import render_template
-from flask import url_for
-from flask import request , redirect
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-
 from datetime import datetime
-from sqlalchemy import Integer , String, DateTime
-
-import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    
-    id = db.Column(Integer , primary_key=True)
-    content = db.Column(String(200),nullable = False)
-    date_created = db.Column(DateTime , default = datetime.utcnow)
-    
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
     def __repr__(self):
         return '<Task %r>' % self.id
-    
-    
-    
-# Index  route
-@app.route("/",methods=["POST","GET"])
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form["content"]
+        task_content = request.form['content']
         new_task = Todo(content=task_content)
-        
+
         try:
             db.session.add(new_task)
             db.session.commit()
-            return redirect("/")
+            return redirect('/')
         except:
-            return "There is issue while adding your Task"
-        
-        
-    else:   
-        tasks = Todo.query.order_by(Todo.date_created).all
-        return render_template("index.html",tasks =tasks)
-    # return "Hemlo Worldd !!"
-    
+            return 'There was an issue adding your task'
 
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask
+# from flask import render_template
+# from flask import url_for
+# from flask import request , redirect
+# from flask_sqlalchemy import SQLAlchemy
+
+# from datetime import datetime
+# from sqlalchemy import Integer , String, DateTime
+
+# import os
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+# db = SQLAlchemy(app)
+
+# class Todo(db.Model):
+    
+#     id = db.Column(Integer , primary_key=True)
+#     content = db.Column(String(200),nullable = False)
+#     date_created = db.Column(DateTime , default = datetime.utcnow)
+    
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
+    
+    
+    
+# # Index  route
+# @app.route("/",methods=["POST","GET"])
+# def index():
+#     if request.method == 'POST':
+#         task_content = request.form["content"]
+#         new_task = Todo(content=task_content)
+        
+#         try:
+#             db.session.add(new_task)
+#             db.session.commit()
+#             return redirect("/")
+#         except:
+#             return "There is issue while adding your Task"
+        
+        
+#     else:   
+#         tasks = Todo.query.order_by(Todo.date_created).all
+#         return render_template("index.html",tasks =tasks)
+#     # return "Hemlo Worldd !!"
+    
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
