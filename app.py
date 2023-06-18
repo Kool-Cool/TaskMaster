1333f04970e061ceb8516889cfe4a4d253921b3b
Kool-Cool <omgrabpoint@gmail.com>
2023-06-18 23:19:36 +0530

    Templates
    Setting up templates , static folder.
    
    Rendering templates in app

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 # Index  route
 @app.route("/")
 def index():
-    return "Hemlo Worldd !!"
+    # return "Hemlo Worldd !!"
+    return render_template("index.html")
 
 
 
